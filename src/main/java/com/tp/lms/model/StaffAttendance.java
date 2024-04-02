47195d31a7c3aa987ab322b2d5885fc6bfbf339b
rahulmaurya11 <rahulmaurya1500@gmail.com>
2024-04-02 10:04:21 +0530

    added staffattendance in model

---
+++ src/main/java/com/tp/lms/model/StaffAttendance.java
@@ -0,0 +1,81 @@
+package com.tp.lms.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ * 
+ * 
+ @author RAHUL */
+
+@Entity
+public class StaffAttendance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "staff_attendance_id")
+    private int id;
+
+    @Column(name= "check_in")
+    private Date checkIn;
+
+    @Column(name= "check_out")
+    private Date checkOut;
+
+
+public enum Presenty {
+    FULLDAY, HALFDAY, ABSENT
+}
+    
+    @Enumerated(EnumType.STRING)
+    @Column(name= "presenty")
+    private Presenty presenty;
+
+    // Getters and Setters
+    
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(Date checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(Date checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    public Presenty getPresenty() {
+        return presenty;
+    }
+
+    public void setPresenty(Presenty presenty) {
+        this.presenty = presenty;
+    }
+
+    
+}
+
+
+    
+

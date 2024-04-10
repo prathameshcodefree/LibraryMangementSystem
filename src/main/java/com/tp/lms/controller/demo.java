1cf1af123be90bd8f1af34964b80ed2d604edb3e
Deepak <deepakprajapati9080@gmail.com>
2024-04-10 12:00:28 +0530

    issue resolve in BookMaster

--- src/main/java/com/tp/lms/controller/demo.java
+++ src/main/java/com/tp/lms/controller/demo.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("demo")
+@RequestMapping("test")
 public class demo {
 	
 	@GetMapping("check")

317641e80158a7ab677509af44e3a02ba1f9a24b
sheela <sheelap795@gmail.com>
2024-04-16 15:30:25 +0530

    added functionality in login

--- src/main/java/com/tp/lms/controller/AuthController.java
+++ src/main/java/com/tp/lms/controller/AuthController.java
@@ -6,10 +6,12 @@
 package com.tp.lms.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tp.lms.dto.LoginRequestDTO;
@@ -29,6 +31,16 @@ public class AuthController {
 	
 	@Autowired
 	StudentService studentService;
+	
+	
+	@GetMapping("converttohash")
+	public String convertToHash(@RequestParam String clearText) {
+		
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String cipherText = passwordEncoder.encode(clearText);
+		
+		return cipherText;
+	}
 	
 	@PostMapping("student/login")
 	public LoginResponseDTO studentLogin(@RequestBody LoginRequestDTO loginRequestDto) {

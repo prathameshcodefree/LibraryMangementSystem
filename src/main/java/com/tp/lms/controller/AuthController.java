910c896c414ede06fa457dc5d8a273a80bda023d
sheela <sheelap795@gmail.com>
2024-04-17 13:10:28 +0530

    Implemented login authentication

--- src/main/java/com/tp/lms/controller/AuthController.java
+++ src/main/java/com/tp/lms/controller/AuthController.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package com.tp.lms.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -53,8 +54,6 @@ public class AuthController {
 			userDto.setFirstName(student.getFirstName());
 			userDto.setMiddleName(student.getMiddleName());
 			userDto.setUserName(student.getUserName());
-			
-			
 			
 			loginResponseDto.setStatus(true);
 			loginResponseDto.setMessage("Login Successfully");
